3727144e2881858f49e1c249aa6ed59cf73153ac
chamalevictor <chamale.victor@gmail.com>
2023-03-22 10:20:10 -0600

    feat: agregadas tablas faltantes

--- database/banco_chinautla.sql
+++ database/banco_chinautla.sql
@@ -2,6 +2,29 @@
 -- Banco Chinautla
 
 
+-- Cliente
+CREATE TABLE tipo_cliente(
+    id_tipo_cliente NUMBER,
+    descripcion VARCHAR2(32),
+    identificador VARCHAR2(32)
+);
+ALTER TABLE tipo_cliente MODIFY (id_tipo_cliente NOT NULL ENABLE);
+ALTER TABLE tipo_cliente ADD CONSTRAINT pk_t_cliente PRIMARY KEY (id_tipo_cliente) ENABLE;
+
+CREATE TABLE cliente(
+    id_cliente NUMBER,
+    nombre VARCHAR2(128),
+    id_tipo_cliente NUMBER,
+    correo VARCHAR2(64),
+    fecha_nac DATE,
+    no_identificacion NUMBER,
+);
+ALTER TABLE cliente MODIFY (id_cliente NOT NULL ENABLE);
+ALTER TABLE cliente ADD CONSTRAINT pk_cliente PRIMARY KEY (id_cliente);
+ALTER TABLE cliente ADD CONSTRAINT fk_cliente_tipo FOREIGN KEY (id_tipo_cliente)
+REFERENCES tipo_cliente (id_tipo_cliente) ENABLE;
+
+
 -- Agencia y Caja
 CREATE TABLE agencia (
     id_agencia NUMBER,
@@ -64,11 +87,8 @@ REFERENCES agencia (id_agencia) ENABLE;
 ALTER TABLE cuenta ADD CONSTRAINT fk_cuenta_cliente FOREIGN KEY (id_cliente)
 REFERENCES cliente (id_cliente) ENABLE;
 
-
-
-
-
-CREATE TABLE tipo_documento /* Boleta, Chequera, Libreta prueba para ronald */( 
+-- Documentos
+CREATE TABLE tipo_documento /* Boleta, Cheque, Libreta */( 
     id_tipo_documento NUMBER,
     descripcion VARCHAR2(32)
 );
@@ -76,33 +96,203 @@ ALTER TABLE tipo_documento MODIFY (id_tipo_documento NOT NULL ENABLE);
 ALTER TABLE tipo_documento ADD CONSTRAINT pk_tipo_documento PRIMARY KEY (id_tipo_documento) ENABLE;
 
 
+CREATE TABLE chequera (
+    id_chequera NUMBER,
+    bloqueado NUMBER(1) -- 0 false, 1 true.
+);
+ALTER TABLE chequera MODIFY (id_chequera NOT NULL ENABLE);
+ALTER TABLE chequera ADD CONSTRAINT pk_chequera PRIMARY KEY (id_chequera) ENABLE;
+
 
 CREATE TABLE documento (
     id_documento NUMBER,
     id_tipo_documento NUMBER,
-    id_cuenta NUMBER,
+    id_tipo_cuenta NUMBER,
+    id_agencia NUMBER,
+    id_correlativo NUMBER,
+    id_aleatorio NUMBER,
+    id_chequera NUMBER,
     bloqueado NUMBER(1) -- 0 false, 1 true.
 );
 ALTER TABLE documento MODIFY (id_documento NOT NULL ENABLE);
 ALTER TABLE documento ADD CONSTRAINT pk_documento PRIMARY KEY (id_documento) ENABLE;
 ALTER TABLE documento ADD CONSTRAINT fk_t_documento FOREIGN KEY (id_tipo_documento)
 REFERENCES tipo_documento (id_tipo_documento) ENABLE;
+ALTER TABLE documento ADD CONSTRAINT fk_t_cuenta FOREIGN KEY (id_tipo_cuenta)
+REFERENCES cuenta (id_tipo_cuenta) ENABLE;
+ALTER TABLE documento ADD CONSTRAINT fk_c_agencia FOREIGN KEY (id_agencia)
+REFERENCES cuenta (id_agencia) ENABLE;
+ALTER TABLE documento ADD CONSTRAINT fk_correlativo FOREIGN KEY (id_correlativo) 
+REFERENCES cuenta (correlativo) ENABLE;
+ALTER TABLE documento ADD CONSTRAINT fk_aleatorio FOREIGN KEY (id_aleatorio) 
+REFERENCES cuenta (aleatorio) ENABLE;
+ALTER TABLE documento ADD CONSTRAINT fk_chequera FOREIGN KEY (id_chequera) 
+REFERENCES chequera (id_chequera) ENABLE;
+
+
+
+-- Usuarios, permisos y modulos.
+CREATE TABLE modulo (
+    id_modulo NUMBER,
+    descripcion VARCHAR2(32)
+);
+ALTER TABLE modulo MODIFY (id_modulo NOT NULL ENABLE) ;
+ALTER TABLE modulo ADD CONSTRAINT pk_modulo PRIMARY KEY (id_modulo) ENABLE;
+
+
+CREATE TABLE permiso (
+    id_permiso NUMBER,
+    descripcion VARCHAR2(32)
+);
+ALTER TABLE permiso MODIFY (id_permiso NOT NULL ENABLE);
+ALTER TABLE permiso ADD CONSTRAINT pk_permiso PRIMARY KEY (id_permiso) ENABLE;
+
+CREATE TABLE permiso_modulo (
+    id_permiso NUMBER,
+    id_modulo NUMBER
+);
+ALTER TABLE permiso_modulo MODIFY (id_permiso NOT NULL, id_modulo NOT NULL ENABLE);
+ALTER TABLE permiso_modulo ADD CONSTRAINT pk_permiso_modulo PRIMARY KEY (id_permiso, id_modulo) ENABLE;
+ALTER TABLE permiso_modulo ADD CONSTRAINT fk_pm_permiso FOREIGN KEY (id_permiso) 
+REFERENCES permiso (id_permiso) ENABLE;
+ALTER TABLE permiso_modulo ADD CONSTRAINT fk_pm_modulo FOREIGN KEY (id_modulo)
+REFERENCES modulo (id_modulo) ENABLE;
+
+
+CREATE TABLE rol (
+    id_rol NUMBER,
+    descripcion VARCHAR2(32)
+);
+ALTER TABLE rol MODIFY (id_rol NOT NULL ENABLE);
+ALTER TABLE rol ADD CONSTRAINT pk_rol PRIMARY KEY (id_rol) ENABLE;
+
+
+CREATE TABLE rol_permiso (
+    id_rol NUMBER,
+    id_permiso NUMBER,
+);
+ALTER TABLE rol_permiso MODIFY (id_rol NOT NULL, id_permiso NOT NULL ENABLE);
+ALTER TABLE rol_permiso ADD CONSTRAINT pk_rp PRIMARY KEY (id_rol, id_permiso) ENABLE;
+ALTER TABLE rol_permiso ADD CONSTRAINT fk_rp_rol FOREIGN KEY (id_rol)
+REFERENCES rol (id_rol);
+ALTER TABLE rol_permiso ADD CONSTRAINT fk_rp_permiso FOREIGN KEY (id_permiso)
+REFERENCES permiso (id_permiso) ENABLE;
+
+
+CREATE TABLE usuario (
+    id_usuario NUMBER,
+    nombre VARCHAR2(32),
+    apellido VARCHAR2(32),
+    correo VARCHAR2(64),
+    id_rol NUMBER
+);
+ALTER TABLE usuario MODIFY (id_usuario NOT NULL ENABLE);
+ALTER TABLE usuario ADD CONSTRAINT pk_usuario PRIMARY KEY (id_usuario) ENABLE;
+ALTER TABLE usuario ADD CONSTRAINT fk_u_rol FOREIGN KEY (id_rol) 
+REFERENCES rol (id_rol) ENABLE;
+
+
+-- Operacion y estado.
+CREATE TABLE estado (
+    id_estado NUMBER, 
+    descripcion VARCHAR2(32)
+);
+ALTER TABLE estado MODIFY (id_estado NOT NULL ENABLE);
+ALTER TABLE estado ADD CONSTRAINT pk_estado PRIMARY KEY (id_estado) ENABLE;
 
 CREATE TABLE tipo_operacion (
     id_tipo_operacion NUMBER,
-    descripcion VARCHAR2(32),
+    descripcion
 );
+ALTER TABLE tipo_operacion MODIFY (id_tipo_operacion NOT NULL ENABLE);
+ALTER TABLE tipo_operacion ADD CONSTRAINT pk_tipo_operacion PRIMARY KEY (id_tipo_operacion) ENABLE;
 
 
 
+CREATE TABLE operacion (
+    id_operacion NUMBER,
+    fecha DATE,
+    id_tipo_operacion NUMBER,
+    id_documento NUMBER,
+    id_estado NUMBER,
+    id_usuario NUMBER,
+    id_caja NUMBER,
+    descripcion VARCHAR2(64),
+    monto NUMBER
+);
+ALTER TABLE operacion MODIFY (id_operacion NOT NULL ENABLE);
+ALTER TABLE operacion ADD CONSTRAINT pk_operacion PRIMARY KEY (id_operacion) ENABLE;
+ALTER TABLE operacion ADD CONSTRAINT fk_o_t_operacion FOREIGN KEY (id_tipo_operacion) 
+REFERENCES tipo_operacion (id_tipo_operacion) ENABLE;
+ALTER TABLE operacion ADD CONSTRAINT fk_o_documento FOREIGN KEY (id_documento)
+REFERENCES documento (id_documento) ENABLE;
+ALTER TABLE operacion ADD CONSTRAINT fk_o_estado FOREIGN KEY (id_estado)
+REFERENCES estado (id_estado) ENABLE;
+ALTER TABLE operacion ADD CONSTRAINT fk_usuario FOREIGN KEY (id_usuario)
+REFERENCES usuario (id_usuario) ENABLE;
+ALTER TABLE operacion ADD CONSTRAINT fk_caja FOREIGN KEY (id_caja)
+REFERENCES caja (id_caja) ENABLE;
 
-CREATE TABLE ();
+
+-- Pago de planillas
+CREATE TABLE planilla (
+    id_planilla NUMBER,
+    monto_inicial NUMBER,
+    id_tipo_cuenta NUMBER,
+    id_agencia NUMBER,
+    id_correlativo NUMBER,
+    id_aleatorio NUMBER,
+    fecha_hora DATE
+);
+ALTER TABLE planilla MODIFY (id_planilla NOT NULL ENABLE);
+ALTER TABLE planilla ADD CONSTRAINT pk_planilla PRIMARY KEY (id_planilla) ENABLE;
+ALTER TABLE planilla ADD CONSTRAINT fk_p_t_cuenta FOREIGN KEY (id_tipo_cuenta) 
+REFERENCES cuenta (id_tipo_cuenta) ENABLE;
+ALTER TABLE planilla ADD CONSTRAINT fk_p_agencia FOREIGN KEY (id_agencia)
+REFERENCES cuenta (id_agencia) ENABLE;
+ALTER TABLE planilla ADD CONSTRAINT fk_p_correlativo FOREIGN KEY (id_correlativo)
+REFERENCES cuenta (correlativo) ENABLE;
+ALTER TABLE planilla ADD CONSTRAINT fk_p_aleatorio FOREIGN KEY (id_aleatorio)
+REFERENCES cuenta (aleatorio) ENABLE;
+
+
+CREATE TABLE planilla_operacion (
+    id_planilla NUMBER,
+    id_operacion NUMBER
+);
+ALTER TABLE planilla_operacion MODIFY (id_planilla NOT NULL, id_operacion NOT NULL ENABLE);
+ALTER TABLE planilla_operacion ADD CONSTRAINT pk_planilla_operacion PRIMARY KEY (id_planilla, id_operacion) ENABLE;
+ALTER TABLE planilla_operacion ADD CONSTRAINT fk_planilla FOREIGN KEY (id_planilla) 
+REFERENCES planilla (id_planilla) ENABLE;
+ALTER TABLE planilla_operacion ADD CONSTRAINT fk_operacion FOREIGN KEY (id_operacion)
+REFERENCES operacion (id_operacion) ENABLE;
+
+
+CREATE TABLE bitacora (
+    id_registro NUMBER,
+    fecha DATE,
+    id_usuario NUMBER,
+    id_tipo_operacion NUMBER,
+    id_modulo NUMBER,
+    id_agencia NUMBER,
+    id_caja NUMBER
+);
+ALTER TABLE bitacora MODIFY (id_registro NOT NULL ENABLE);
+ALTER TABLE bitacora ADD CONSTRAINT pk_bitacora PRIMARY KEY (id_registro) ENABLE;
+ALTER TABLE bitacora ADD CONSTRAINT fk_b_usuario FOREIGN KEY (id_usuario)
+REFERENCES usuario (id_usuario) ENABLE;
+ALTER TABLE bitacora ADD CONSTRAINT fk_b_t_operacion FOREIGN KEY (id_tipo_operacion)
+REFERENCES tipo_operacion (id_tipo_operacion) ENABLE;
+ALTER TABLE bitacora ADD CONSTRAINT fk_b_modulo FOREIGN KEY (id_modulo) 
+REFERENCES modulo (id_modulo) ENABLE;
+ALTER TABLE bitacora ADD CONSTRAINT fk_b_agencia FOREIGN KEY (id_agencia)
+REFERENCES agencia (id_agencia) ENABLE;
+ALTER TABLE bitacora ADD CONSTRAINT fk_b_caja FOREIGN KEY (id_caja)
+REFERENCES caja (id_caja) ENABLE;
 
 
 
-
-
--- EStados de cuenta por rango de fechas y cuenta
+-- Estados de cuenta por rango de fechas y cuenta
 SELECT 
 op.fecha, t_op.descripcion, t_do.descripcion, op.id_documento, es.descripcion, op.id_cheque, op.descripcion, op.monto
 FROM
@@ -152,3 +342,6 @@ WHERE
 
 
 -- Estadisticas de operaciones por agencia: fecha de operacion, tipo de operacion (credito/debito) y monto total.
+
+
+-- pendientes
