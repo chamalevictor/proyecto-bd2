1e4142967b854b35accae23a6380c558a11ee27d
chamalevictor <chamale.victor@gmail.com>
2023-03-23 22:15:41 -0600

    feat: terminado creacion de tablas

--- database/banco_chinautla.sql
+++ database/banco_chinautla.sql
@@ -17,7 +17,7 @@ CREATE TABLE cliente(
     id_tipo_cliente NUMBER,
     correo VARCHAR2(64),
     fecha_nac DATE,
-    no_identificacion NUMBER,
+    no_identificacion NUMBER
 );
 ALTER TABLE cliente MODIFY (id_cliente NOT NULL ENABLE);
 ALTER TABLE cliente ADD CONSTRAINT pk_cliente PRIMARY KEY (id_cliente);
@@ -29,7 +29,7 @@ REFERENCES tipo_cliente (id_tipo_cliente) ENABLE;
 CREATE TABLE agencia (
     id_agencia NUMBER,
     nombre VARCHAR2(100),
-    ubicación VARCHAR2(100),
+    ubicacion VARCHAR2(100),
     telefono NUMBER
 );
 ALTER TABLE agencia MODIFY (id_agencia NOT NULL ENABLE);
@@ -61,7 +61,7 @@ CREATE TABLE tipo_cuenta (
     id_interes NUMBER
 );
 ALTER TABLE tipo_cuenta MODIFY (id_tipo_cuenta NOT NULL ENABLE);
-ALTER TABLE tipo_cuenta ADD CONSTRAINT pk_t_cuenta PRIMARY KEY (id_tipo_cuenta)
+ALTER TABLE tipo_cuenta ADD CONSTRAINT pk_t_cuenta PRIMARY KEY (id_tipo_cuenta);
 ALTER TABLE tipo_cuenta ADD CONSTRAINT fk_interes FOREIGN KEY (id_interes)
 REFERENCES interes (id_interes) ENABLE;
 
@@ -76,10 +76,10 @@ CREATE TABLE cuenta (
     activa NUMBER(1), -- 0 false, 1 true
     firma_1 VARCHAR2(128),
     firma_2 VARCHAR2(128),
-    firma_2 VARCHAR2(128),
+    firma_3 VARCHAR2(128)
 );
 ALTER TABLE cuenta MODIFY (id_tipo_cuenta, id_agencia, correlativo, aleatorio NOT NULL ENABLE);
-ALTER TABLE cuenta ADD CONSTRAINT pk_cuenta PRIMARY KEY (id_tipo_cuenta, id_agencia, correlativo, aleatorio)
+ALTER TABLE cuenta ADD CONSTRAINT pk_cuenta PRIMARY KEY (id_tipo_cuenta, id_agencia, correlativo, aleatorio) ENABLE;
 ALTER TABLE cuenta ADD CONSTRAINT fk_cuenta_t_cuenta FOREIGN KEY (id_tipo_cuenta) 
 REFERENCES tipo_cuenta (id_tipo_cuenta) ENABLE;
 ALTER TABLE cuenta ADD CONSTRAINT fk_cuenta_agencia FOREIGN KEY (id_agencia)
@@ -87,7 +87,7 @@ REFERENCES agencia (id_agencia) ENABLE;
 ALTER TABLE cuenta ADD CONSTRAINT fk_cuenta_cliente FOREIGN KEY (id_cliente)
 REFERENCES cliente (id_cliente) ENABLE;
 
--- Documentos
+-- Documentos 
 CREATE TABLE tipo_documento /* Boleta, Cheque, Libreta */( 
     id_tipo_documento NUMBER,
     descripcion VARCHAR2(32)
@@ -116,20 +116,13 @@ CREATE TABLE documento (
 );
 ALTER TABLE documento MODIFY (id_documento NOT NULL ENABLE);
 ALTER TABLE documento ADD CONSTRAINT pk_documento PRIMARY KEY (id_documento) ENABLE;
-ALTER TABLE documento ADD CONSTRAINT fk_t_documento FOREIGN KEY (id_tipo_documento)
-REFERENCES tipo_documento (id_tipo_documento) ENABLE;
-ALTER TABLE documento ADD CONSTRAINT fk_t_cuenta FOREIGN KEY (id_tipo_cuenta)
-REFERENCES cuenta (id_tipo_cuenta) ENABLE;
-ALTER TABLE documento ADD CONSTRAINT fk_c_agencia FOREIGN KEY (id_agencia)
-REFERENCES cuenta (id_agencia) ENABLE;
-ALTER TABLE documento ADD CONSTRAINT fk_correlativo FOREIGN KEY (id_correlativo) 
-REFERENCES cuenta (correlativo) ENABLE;
-ALTER TABLE documento ADD CONSTRAINT fk_aleatorio FOREIGN KEY (id_aleatorio) 
-REFERENCES cuenta (aleatorio) ENABLE;
+ALTER TABLE documento ADD CONSTRAINT fk_d_t_documento FOREIGN KEY (id_tipo_documento)
+REFERENCES tipo_documento (id_tipo_documento);
+ALTER TABLE documento ADD CONSTRAINT fk_documento_cuenta
+FOREIGN KEY (id_tipo_cuenta, id_agencia, id_correlativo, id_aleatorio)
+REFERENCES cuenta (id_tipo_cuenta, id_agencia, correlativo, aleatorio) ENABLE;
 ALTER TABLE documento ADD CONSTRAINT fk_chequera FOREIGN KEY (id_chequera) 
 REFERENCES chequera (id_chequera) ENABLE;
-
-
 
 -- Usuarios, permisos y modulos.
 CREATE TABLE modulo (
@@ -138,7 +131,6 @@ CREATE TABLE modulo (
 );
 ALTER TABLE modulo MODIFY (id_modulo NOT NULL ENABLE) ;
 ALTER TABLE modulo ADD CONSTRAINT pk_modulo PRIMARY KEY (id_modulo) ENABLE;
-
 
 CREATE TABLE permiso (
     id_permiso NUMBER,
@@ -158,6 +150,7 @@ REFERENCES permiso (id_permiso) ENABLE;
 ALTER TABLE permiso_modulo ADD CONSTRAINT fk_pm_modulo FOREIGN KEY (id_modulo)
 REFERENCES modulo (id_modulo) ENABLE;
 
+-- permiso_modulo listo
 
 CREATE TABLE rol (
     id_rol NUMBER,
@@ -166,10 +159,9 @@ CREATE TABLE rol (
 ALTER TABLE rol MODIFY (id_rol NOT NULL ENABLE);
 ALTER TABLE rol ADD CONSTRAINT pk_rol PRIMARY KEY (id_rol) ENABLE;
 
-
 CREATE TABLE rol_permiso (
     id_rol NUMBER,
-    id_permiso NUMBER,
+    id_permiso NUMBER
 );
 ALTER TABLE rol_permiso MODIFY (id_rol NOT NULL, id_permiso NOT NULL ENABLE);
 ALTER TABLE rol_permiso ADD CONSTRAINT pk_rp PRIMARY KEY (id_rol, id_permiso) ENABLE;
@@ -192,7 +184,7 @@ ALTER TABLE usuario ADD CONSTRAINT fk_u_rol FOREIGN KEY (id_rol)
 REFERENCES rol (id_rol) ENABLE;
 
 
--- Operacion y estado.
+-- Operacion y estado. 
 CREATE TABLE estado (
     id_estado NUMBER, 
     descripcion VARCHAR2(32)
@@ -202,11 +194,10 @@ ALTER TABLE estado ADD CONSTRAINT pk_estado PRIMARY KEY (id_estado) ENABLE;
 
 CREATE TABLE tipo_operacion (
     id_tipo_operacion NUMBER,
-    descripcion
+    descripcion VARCHAR2(32)
 );
 ALTER TABLE tipo_operacion MODIFY (id_tipo_operacion NOT NULL ENABLE);
 ALTER TABLE tipo_operacion ADD CONSTRAINT pk_tipo_operacion PRIMARY KEY (id_tipo_operacion) ENABLE;
-
 
 
 CREATE TABLE operacion (
@@ -246,15 +237,9 @@ CREATE TABLE planilla (
 );
 ALTER TABLE planilla MODIFY (id_planilla NOT NULL ENABLE);
 ALTER TABLE planilla ADD CONSTRAINT pk_planilla PRIMARY KEY (id_planilla) ENABLE;
-ALTER TABLE planilla ADD CONSTRAINT fk_p_t_cuenta FOREIGN KEY (id_tipo_cuenta) 
-REFERENCES cuenta (id_tipo_cuenta) ENABLE;
-ALTER TABLE planilla ADD CONSTRAINT fk_p_agencia FOREIGN KEY (id_agencia)
-REFERENCES cuenta (id_agencia) ENABLE;
-ALTER TABLE planilla ADD CONSTRAINT fk_p_correlativo FOREIGN KEY (id_correlativo)
-REFERENCES cuenta (correlativo) ENABLE;
-ALTER TABLE planilla ADD CONSTRAINT fk_p_aleatorio FOREIGN KEY (id_aleatorio)
-REFERENCES cuenta (aleatorio) ENABLE;
-
+ALTER TABLE planilla ADD CONSTRAINT fk_planilla_cuenta
+FOREIGN KEY (id_tipo_cuenta, id_agencia, id_correlativo, id_aleatorio)
+REFERENCES cuenta (id_tipo_cuenta, id_agencia, correlativo, aleatorio) ENABLE;
 
 CREATE TABLE planilla_operacion (
     id_planilla NUMBER,
@@ -268,6 +253,7 @@ ALTER TABLE planilla_operacion ADD CONSTRAINT fk_operacion FOREIGN KEY (id_opera
 REFERENCES operacion (id_operacion) ENABLE;
 
 
+-- Safe
 CREATE TABLE bitacora (
     id_registro NUMBER,
     fecha DATE,
@@ -291,7 +277,7 @@ ALTER TABLE bitacora ADD CONSTRAINT fk_b_caja FOREIGN KEY (id_caja)
 REFERENCES caja (id_caja) ENABLE;
 
 
-
+/*
 -- Estados de cuenta por rango de fechas y cuenta
 SELECT 
 op.fecha, t_op.descripcion, t_do.descripcion, op.id_documento, es.descripcion, op.id_cheque, op.descripcion, op.monto
@@ -345,3 +331,5 @@ WHERE
 
 
 -- pendientes
+        completar primeros reportes.
+*/
