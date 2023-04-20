8be195d20c417e2151294bc4cad7ed9050d1674b
victorchamale <victor.chamale@telus.com>
2023-04-20 12:55:53 -0600

    feat: primera fase seeding

---
+++ database/db-seeding.sql
@@ -0,0 +1,43 @@
+-- Seeding
+
+-- Tabla para Roles de Usuario
+-- Secuencia de Roles
+
+CREATE SEQUENCE rol_seq
+    INCREMENT BY 1
+    START WITH 1
+    MINVALUE 1
+    MAXVALUE 1000
+    NOCYCLE
+    CACHE 2;
+commit;
+
+-- Inserts
+INSERT INTO rol VALUES (rol_seq.nextval, 'Administrador');
+INSERT INTO rol VALUES (rol_seq.nextval, 'Gerencia');
+INSERT INTO rol VALUES (rol_seq.nextval, 'Supervisor');
+INSERT INTO rol VALUES (rol_seq.nextval, 'Representante de Servicio al Cliente');
+INSERT INTO rol VALUES (rol_seq.nextval, 'Cajero');
+commit;
+
+select * from rol;
+-- Modulos
+-- Secuencia para ids de Modulos
+
+CREATE SEQUENCE modulo_seq
+    INCREMENT BY 1
+    START WITH 1
+    MINVALUE 1
+    MAXVALUE 1000
+    NOCYCLE
+    CACHE 2;
+commit;
+
+INSERT INTO modulo (modulo_seq.nextval, '')
+
+
+
+-- TODO: Modificar usuarios y cuentas agregando un campo para saber si estan activos o no.
+
+
+
